b072b31945c3ac29738a0437ff6fe6df82b532b7
Natacha Herth <info@natachaherth.ch>
2020-03-30 14:48:50 +0200

    Dashboard with designed components

--- src/stubs/backend/resources/views/backend/dashboard.blade.php
+++ src/stubs/backend/resources/views/backend/dashboard.blade.php
@@ -7,4 +7,21 @@
   <h2>Welcome to the dashboard</h2>
   <p>This is the default layout</p>
 
+  <hr/>
+
+  <x-search action="#" is-advanced>
+    Here add the advanced search !
+  </x-search>
+
+  <hr/>
+
+  <a class="btn btn-roun btn-sm"><i class="icon icon-trash"></i></a>
+  <a class="btn btn-roun "><i class="icon icon-trash"></i></a>
+  <a class="btn btn-roun btn-lg"><i class="icon icon-trash"></i></a>
+  <a class="btn btn-roun disabled"><i class="icon icon-trash"></i></a>
+  <a class="btn btn-roun-primary"><i class="icon icon-trash"></i></a>
+  <a class="btn btn-roun-outline-primary"><i class="icon icon-trash"></i></a>
+
+  <hr/>
+
 @endsection
