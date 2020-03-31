456eeffe415df2b15125d0286c1ac4f259b8ab6c
Natacha Herth <info@natachaherth.ch>
2020-03-31 09:25:52 +0200

    Corr logout

--- src/stubs/backend/routes/backend.php
+++ src/stubs/backend/routes/backend.php
@@ -17,6 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('backend.dashboard');
 })->name('dashboard');
-
-// Logout
-Route::post('/logout','Auth\LoginController@logout')->name('logout');
