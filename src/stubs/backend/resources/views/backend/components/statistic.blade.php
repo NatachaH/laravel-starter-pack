679b01ed83b2ca9a562c0189141f75cad4686d61
Natacha Herth <info@natachaherth.ch>
2020-03-31 15:48:54 +0200

    Statistic title

--- src/stubs/backend/resources/views/backend/components/statistic.blade.php
+++ src/stubs/backend/resources/views/backend/components/statistic.blade.php
@@ -1,7 +1,7 @@
 <div {{ $attributes->merge(['class' => 'card statistic']) }} >
     <div class="card-body statistic-body border-{{ $color }}">
       <i class="icon {{ $icon }} text-{{ $color }}"></i>
-      <p class="card-title">{{ $title }}</p>
+      <p class="statistic-title">{{ $title }}</p>
       <p class="statistic-value text-{{ $color }}">{{ $value }}</p>
     </div>
   </div>
