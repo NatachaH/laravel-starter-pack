76a040c0e1478b55e6a14ca2164ab603092ce8c5
Natacha Herth <info@natachaherth.ch>
2020-03-31 09:53:35 +0200

    Clean button in listing component

--- src/stubs/backend/resources/views/backend/components/listing.blade.php
+++ src/stubs/backend/resources/views/backend/components/listing.blade.php
@@ -7,11 +7,11 @@
         <div>
 
             @if(Route::has($route.'.export') && Auth::user()->can('export', $model))
-                <a href="{{ route($route.'.export') }}" class="btn btn-outline-secondary"><i class="icon icon-download"></i> @lang('backend.export')</a>
+                <a href="{{ route($route.'.export') }}" class="btn btn-outline-secondary rounded-pill"><i class="icon icon-download"></i> @lang('backend.export')</a>
             @endif
 
             @if(Route::has($route.'.create') && Auth::user()->can('create', $model))
-                <a href="{{ route($route.'.create') }}" class="btn btn-brand"><i class="icon icon-plus"></i> @lang('backend.add')</a>
+                <a href="{{ route($route.'.create') }}" class="btn btn-brand rounded-pill"><i class="icon icon-plus"></i> @lang('backend.add')</a>
             @endif
 
         </div>
@@ -62,23 +62,23 @@
                             <td class="td-fit">
 
                                 @if(Route::has($route.'.show') && Auth::user()->can('view', $item))
-                                    <a href="{{ route($route.'.show', $item->id) }}" class="btn btn-round"><i class="icon icon-preview" alt="@lang('backend.preview')"></i></a>
+                                    <a href="{{ route($route.'.show', $item->id) }}" class="btn btn-default btn-sm rounded-circle"><i class="icon icon-preview" alt="@lang('backend.preview')"></i></a>
                                 @endif
 
                                 @if(Route::has($route.'.edit') && Auth::user()->can('update', $item))
-                                    <a href="{{ route($route.'.edit', $item->id) }}" class="btn btn-round"><i class="icon icon-pencil" alt="@lang('backend.edit')"></i></a>
+                                    <a href="{{ route($route.'.edit', $item->id) }}" class="btn btn-default btn-sm rounded-circle"><i class="icon icon-pencil" alt="@lang('backend.edit')"></i></a>
                                 @endif
 
                                 @if(Route::has($route.'.destroy') && Auth::user()->can('delete', $item))
-                                    <a href="{{ route($route.'.destroy', $item->id) }}" class="btn btn-round"><i class="icon icon-trash" alt="@lang('backend.delete')"></i></a>
+                                    <a href="{{ route($route.'.destroy', $item->id) }}" class="btn btn-default btn-sm rounded-circle"><i class="icon icon-trash" alt="@lang('backend.delete')"></i></a>
                                 @endif
 
                                 @if(Route::has($route.'.restore') && Auth::user()->can('restore', $item))
-                                    <a href="{{ route($route.'.restore', $item->id) }}" class="btn btn-round"><i class="icon icon-time-reverse" alt="@lang('backend.restore')"></i></a>
+                                    <a href="{{ route($route.'.restore', $item->id) }}" class="btn btn-default btn-sm rounded-circle"><i class="icon icon-time-reverse" alt="@lang('backend.restore')"></i></a>
                                 @endif
 
                                 @if(Route::has($route.'.forceDelete') && Auth::user()->can('forceDelete', $item))
-                                    <a href="{{ route($route.'.forceDelete', $item->id) }}" class="btn btn-round"><i class="icon icon-trash" alt="@lang('backend.force-delete')"></i></a>
+                                    <a href="{{ route($route.'.forceDelete', $item->id) }}" class="btn btn-default btn-sm rounded-circle"><i class="icon icon-trash" alt="@lang('backend.force-delete')"></i></a>
                                 @endif
 
                             </td>
