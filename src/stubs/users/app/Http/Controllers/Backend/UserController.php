4b30e7620d454787e36108b6b69bb9b95fef880f
Natacha Herth <info@natachaherth.ch>
2020-04-01 15:37:15 +0200

    Controller in UserController

--- src/stubs/users/app/Http/Controllers/Backend/UserController.php
+++ src/stubs/users/app/Http/Controllers/Backend/UserController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Backend;
 
-use App\User;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+
+use App\User;
 
 class UserController extends Controller
 {
