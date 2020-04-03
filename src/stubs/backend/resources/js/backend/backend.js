fed8468b8b9f5c5e12b81fb683c2c941b95e5585
Natacha Herth <info@natachaherth.ch>
2020-04-03 13:15:31 +0200

    corr js open confirm modal

--- src/stubs/backend/resources/js/backend/backend.js
+++ src/stubs/backend/resources/js/backend/backend.js
@@ -18,10 +18,10 @@ $('#toastNotification .toast').toast('show');
 
 // Open Confirm Modal
 var confirmModals = document.getElementsByClassName('modal-confirm');
-confirmModals.forEach(function(modal){
+Array.prototype.forEach.call(confirmModals, function(modal) {
   $(modal).on('show.bs.modal', function (event) {
     var button = event.relatedTarget
     var action = button.getAttribute('data-action');
-    confirmDeleteModal.querySelector('form').action = action
+    modal.querySelector('form').action = action
   });
 });
