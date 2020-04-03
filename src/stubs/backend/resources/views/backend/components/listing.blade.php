95b1e102c789f8b0f4d03d5a22c8fa6e450fd6ce
Natacha Herth <info@natachaherth.ch>
2020-04-03 21:13:27 +0200

    Listing pagination + total

--- src/stubs/backend/resources/views/backend/components/listing.blade.php
+++ src/stubs/backend/resources/views/backend/components/listing.blade.php
@@ -95,7 +95,7 @@
     </div>
 
     @if($hasPagination)
-        <div class="listing-footer d-flex">
+        <div class="listing-footer d-flex align-items-center">
             {{ $items->appends(request()->only('search'))->links('backend.components.pagination') }}
             <span class="listing-total ml-auto">@lang('backend.listing.total') {{$items->count().'/'.$items->total()}}</span>
         </div>
