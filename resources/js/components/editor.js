81f646febe54253e9088c4c6ee19218b556c9bd2
Natacha Herth <info@natachaherth.ch>
2020-04-09 16:36:26 +0200

    basic promp url check

--- resources/js/components/editor.js
+++ resources/js/components/editor.js
@@ -33,7 +33,34 @@ Array.prototype.forEach.call(editors, function(el, i) {
     var ql = new Quill(editor, {
       modules: {
           toolbar: {
-              container: toolbar
+              container: toolbar,
+              handlers: {
+                // handlers object will be merged with default handlers object
+                'link': function(value) {
+                  if (value) {
+                    var href = prompt('URL');
+
+                    var patternUrl = new RegExp('^(https?:\\/\\/)?'+ // protocol
+                       '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+                       '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+                       '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+                       '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+                       '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+
+                    var patternEmail = new RegExp(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+
+                    if(!!patternUrl.test(href))
+                    {
+                      this.quill.format('link', href);
+                    } else if (!!patternEmail.test(href)) {
+                      this.quill.format('link', 'mailto:'+href);
+                    }
+
+                  } else {
+                    this.quill.format('link', false);
+                  }
+                }
+              }
           },
           keyboard: {
             bindings: {
