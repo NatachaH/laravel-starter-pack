8c9ead5372154667abcfbfe214633f1461a06976
Natacha Herth <info@natachaherth.ch>
2020-04-13 13:49:10 +0200

    button form as save

--- resources/views/partials/form-footer.blade.php
+++ resources/views/partials/form-footer.blade.php
@@ -1,6 +1,6 @@
 <div class="form-footer d-flex align-items-center justify-content-between">
   <div>
-    <button type="submit" class="btn btn-brand btn-sm rounded-pill">@lang('sp::action.create')</button>
+    <button type="submit" class="btn btn-brand btn-sm rounded-pill">@lang('sp::action.save')</button>
     <a href="{{ route('backend.users.index') }}" class="btn btn-outline-secondary btn-sm rounded-pill">@lang('sp::action.cancel')</a>
   </div>
   <p class="text-muted">@lang('sp::help.required')</p>
