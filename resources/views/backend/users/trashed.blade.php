20dd4ccbc4712d437a0dd8dc123146ce909c5055
Natacha Herth <info@natachaherth.ch>
2020-04-14 18:20:37 +0200

    Trashed user

--- resources/views/backend/users/trashed.blade.php
+++ resources/views/backend/users/trashed.blade.php
@@ -6,6 +6,6 @@
 
   <x-sp-search route="#"/>
 
-  <x-sp-listing :title="trans_choice('backend.model.user',2)" model="User" route="backend.users" folder="sp::backend.users" header="name|email" :items="$users" show-id show-dates />
+  <x-sp-listing :title="trans_choice('backend.model.user',2)" model="App\User" route="backend.users" folder="sp::backend.users" header="name|email" :items="$users" show-id show-dates />
 
 @endsection
