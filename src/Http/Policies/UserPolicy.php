740c2e727febe6ca3c095dddb6cfe0aee649eeac
Natacha Herth <info@natachaherth.ch>
2020-04-15 11:22:56 +0200

    user policy

--- src/Http/Policies/UserPolicy.php
+++ src/Http/Policies/UserPolicy.php
@@ -86,7 +86,7 @@ class UserPolicy
      */
     public function delete(User $user, User $model)
     {
-        return $user->hasAccess('user','delete') && !$model->trashed() && (!$model->hasRoles('superadmin') || $user->hasRoles('superadmin'));
+        return $user->hasAccess('user','delete') && !$model->trashed() && ($user->id !== $model->id) && (!$model->hasRoles('superadmin') || $user->hasRoles('superadmin'));
     }
 
     /**
