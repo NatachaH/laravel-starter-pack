6904d43a958f726f3928f5e3c0c4726a32f89930
Natacha Herth <info@natachaherth.ch>
2020-04-21 15:39:40 +0200

    c

--- stubs/content/global/resources/views/backend/contents/show.blade.php
+++ stubs/content/global/resources/views/backend/contents/show.blade.php
@@ -7,7 +7,7 @@
     <div class="page-header">
       <h2>{{ ${{ NAME }}->title }}</h2>
       @if(Route::has('backend.{{ PNAME }}.edit') && Auth::user()->can('update', ${{ NAME }}))
-          <a href="{{ route('backend.{{ PNAME }}.edit', ${{ NAME }}->id) }}" class="btn btn-outline-primary" aria-label="@lang('sp::action.edit')"><i class="icon icon-pencil"></i> @lang('sp::action.edit')</a>
+          <a href="{{ route('backend.{{ PNAME }}.edit', ${{ NAME }}->id) }}" class="btn btn-outline-primary rounded-pill" aria-label="@lang('sp::action.edit')"><i class="icon icon-pencil"></i> @lang('sp::action.edit')</a>
       @endif
     </div>
 
