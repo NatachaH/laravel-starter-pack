696f5eaf8edf2650afffc304a811b26eb2b7a795
Natacha Herth <info@natachaherth.ch>
2020-04-23 14:28:43 +0200

    Corr toast and version

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -1,4 +1,4 @@
-<div class="toast {{ 'toast-'.$color }}" role="alert" aria-live="assertive" aria-atomic="true" data-delay="{{ $delay }}" data-autohide="true">
+<div {{ $attributes->merge(['class' => 'toast toast-'.$color]) }} role="alert" aria-live="assertive" aria-atomic="true" data-delay="{{ $delay }}" data-autohide="true">
   <div class="toast-body">
     <i class="icon {{ $icon }} mr-2"></i> {{ $message }}
   </div>
