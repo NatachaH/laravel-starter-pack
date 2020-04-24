8f8cb80e69853050635715be6a6d3eb73d9607b7
Natacha Herth <info@natachaherth.ch>
2020-04-24 14:21:55 +0200

    Add a permission translation

---
+++ stubs/backend/resources/lang/en/permission.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Permission Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for the permission list
+    |
+    */
+
+    'user'  => 'User',
+    'role'  => 'Role',
+
+];
