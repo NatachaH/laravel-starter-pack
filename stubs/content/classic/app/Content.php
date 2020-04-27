883b0e703c6a66bc7e95430720fd7e2a3f11633e
Natacha Herth <info@natachaherth.ch>
2020-04-27 10:56:53 +0200

    add trait Searchable

--- stubs/content/classic/app/Content.php
+++ stubs/content/classic/app/Content.php
@@ -5,11 +5,13 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 use Nh\Mediable\Traits\Mediable;
+use Nh\Searchable\Traits\Searchable;
 
 class {{ UCNAME }} extends Model
 {
 
     use Mediable;
+    use Searchable;
 
     /**
      * The attributes that are mass assignable.
@@ -25,5 +27,14 @@ class {{ UCNAME }} extends Model
      * @var int
      */
     protected $perPage = 10;
+
+    /**
+     * The searchable columns.
+     *
+     * @var array
+     */
+    protected $searchable = [
+      'title', 'subtitle', 'description'
+    ];
 
 }
--- stubs/content/softdelete/app/Content.php
+++ stubs/content/softdelete/app/Content.php
@@ -6,11 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 use Nh\Mediable\Traits\Mediable;
+use Nh\Searchable\Traits\Searchable;
 
 class {{ UCNAME }} extends Model
 {
     use SoftDeletes;
-      use Mediable;
+    use Mediable;
+    use Searchable;
 
     /**
      * The attributes that are mass assignable.
@@ -26,5 +28,14 @@ class {{ UCNAME }} extends Model
      * @var int
      */
     protected $perPage = 10;
+
+    /**
+     * The searchable columns.
+     *
+     * @var array
+     */
+    protected $searchable = [
+      'title', 'subtitle', 'description'
+    ];
 
 }
