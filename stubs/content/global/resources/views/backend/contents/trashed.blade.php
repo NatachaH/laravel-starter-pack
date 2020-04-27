19a37d6489933fc0e942b6718f74931d99f8f685
Natacha Herth <info@natachaherth.ch>
2020-04-27 15:23:08 +0200

    corr trashed view search component

--- stubs/content/global/resources/views/backend/contents/trashed.blade.php
+++ stubs/content/global/resources/views/backend/contents/trashed.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-  <x-sp-search route="#"/>
+  <x-sp-search key="{{ PNAME }}" action="backend.{{ PNAME }}.search"/>
 
   <x-sp-listing :title="trans_choice('backend.model.{{ NAME }}',2)" model="App\{{ UCNAME }}" route="backend.{{ PNAME }}" header="title" :items="${{ PNAME }}" show-id show-dates />
 
