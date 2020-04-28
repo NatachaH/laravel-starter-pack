c0ce566367236e6aeaa5aa28c4ceb1e8dc1ba1e6
Natacha Herth <info@natachaherth.ch>
2020-04-28 10:22:12 +0200

    permission table td-fit

--- resources/views/backend/permissions/table.blade.php
+++ resources/views/backend/permissions/table.blade.php
@@ -2,25 +2,25 @@
   <table class="table mb-0">
 
       <thead>
-          <th></th>
+          <th class="td-fit"></th>
           <th class="text-center">@lang('sp::action.view')</th>
           <th class="text-center">@lang('sp::action.create')</th>
           <th class="text-center">@lang('sp::action.update')</th>
           <th class="text-center">@lang('sp::action.delete')</th>
           <th class="text-center">@lang('sp::action.restore')</th>
-          <th class="text-center">@lang('sp::action.force-delete')</th>
+          <th class="td-fit text-center">@lang('sp::action.force-delete')</th>
       </thead>
 
       <tbody>
         @foreach ($permissions as $key => $permission)
           <tr>
-            <td><b>{{ \Lang::has('permission.'.$key) ? trans_choice('permission.'.$key,1) : $key  }}</b></td>
+            <td class="td-fit"><b>{{ \Lang::has('permission.'.$key) ? trans_choice('permission.'.$key,1) : $key  }}</b></td>
             <td class="text-center">@include('sp::backend.permissions.includes.icon', ['permission' => $permission->firstWhere('action','view')])</td>
             <td class="text-center">@include('sp::backend.permissions.includes.icon', ['permission' => $permission->firstWhere('action','create')])</td>
             <td class="text-center">@include('sp::backend.permissions.includes.icon', ['permission' => $permission->firstWhere('action','update')])</td>
             <td class="text-center">@include('sp::backend.permissions.includes.icon', ['permission' => $permission->firstWhere('action','delete')])</td>
             <td class="text-center">@include('sp::backend.permissions.includes.icon', ['permission' => $permission->firstWhere('action','restore')])</td>
-            <td class="text-center">@include('sp::backend.permissions.includes.icon', ['permission' => $permission->firstWhere('action','force-delete')])</td>
+            <td class="td-fit text-center">@include('sp::backend.permissions.includes.icon', ['permission' => $permission->firstWhere('action','force-delete')])</td>
           </tr>
         @endforeach
       </tbody>
