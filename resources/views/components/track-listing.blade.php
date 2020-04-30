fcd7ae292c4b3ff3f4f38ff0106eb01556fcd0ff
Natacha Herth <info@natachaherth.ch>
2020-04-30 16:55:54 +0200

    if not by user

--- resources/views/components/track-listing.blade.php
+++ resources/views/components/track-listing.blade.php
@@ -5,7 +5,7 @@
           @lang('trackable.event.'.$track->name)
         </span>
         <p class="mb-1"><b>@choice('backend.model.'.$track->model,1)</b> - {{ $track->description }}</p>
-        <small class="text-muted d-block"><i class="icon-stopwatch"></i> {{ $track->created_at->diffForhumans() }} <i class="icon-user ml-2"></i> {{ $track->user->name }}</small>
+        <small class="text-muted d-block"><i class="icon-stopwatch"></i> {{ $track->created_at->diffForhumans() }} <i class="icon-user ml-2"></i> {{ $track->user ? $track->user->name : '-' }}</small>
     </li>
   @endforeach
 </ul>
