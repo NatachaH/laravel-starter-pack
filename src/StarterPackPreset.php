564bc5fa05a0aedfc8e8c0d424291d0691e8fe33
Natacha Herth <info@natachaherth.ch>
2020-05-07 15:41:35 +0200

    corr preset

--- src/StarterPackPreset.php
+++ src/StarterPackPreset.php
@@ -4,6 +4,7 @@ namespace Nh\StarterPack;
 
 use Laravel\Ui\Presets\Preset;
 use Illuminate\Filesystem\Filesystem;
+use Symfony\Component\Finder\SplFileInfo;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
