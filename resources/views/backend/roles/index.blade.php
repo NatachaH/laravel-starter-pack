5ec293d30304940cfc71c1dd1261673684f01747
Natacha Herth <info@natachaherth.ch>
2020-12-03 13:06:54 +0100

    Corr for search user / role

--- resources/views/backend/roles/index.blade.php
+++ resources/views/backend/roles/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-  <x-sp-search key="roles" route="backend.roles"/>
+  <x-sp-search key="roles" model="App\Models\Role" route="backend.roles"/>
 
   <x-sp-listing :items="$roles" header="name" model="App\Models\Role" route="backend.roles" folder="sp::backend.roles" show-id :show-dates="Auth::user()->hasRoles('superadmin')" />
 
--- resources/views/backend/users/index.blade.php
+++ resources/views/backend/users/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-  <x-sp-search key="users" route="backend.users"/>
+  <x-sp-search key="users"  model="App\Models\User" route="backend.users"/>
 
   <x-sp-listing :items="$users" header="name|email|role" model="App\Models\User" route="backend.users" folder="sp::backend.users" show-id :show-dates="Auth::user()->hasRoles('superadmin')" />
 
