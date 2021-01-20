838b9bda58552a566a35f0a2b1d57665b4bab569
Natacha Herth <info@natachaherth.ch>
2021-01-20 14:55:13 +0100

    Corr view trashed for users

--- resources/views/backend/users/trashed.blade.php
+++ resources/views/backend/users/trashed.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-  <x-sp-search key="users" route="backend.users"/>
+  <x-sp-search model="App\Models\User" key="users" route="backend.users"/>
 
   <x-sp-listing :title="trans_choice('backend.model.user',2)" :items="$users" header="name|email|role" model="App\Models\User" route="backend.users" folder="sp::backend.users" show-id show-dates />
 
