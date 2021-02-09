1da60907a45e52a24e02ba8bd14ae53d2ade10b7
Natacha Herth <info@natachaherth.ch>
2021-02-09 15:39:34 +0100

    Corr App track model in user

--- stubs/sp/app/Models/User.php
+++ stubs/sp/app/Models/User.php
@@ -80,6 +80,6 @@ class User extends Authenticatable
      */
     public function activityTracks()
     {
-        return $this->hasMany('\Nh\Trackable\Models\Track');
+        return $this->hasMany('\App\Models\Track');
     }
 }
