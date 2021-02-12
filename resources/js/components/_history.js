8470c0237ff9759caf77aa36fcb4c97377009f0c
Natacha Herth <info@natachaherth.ch>
2021-02-12 16:14:59 +0100

    Tooltip history trigger as click

--- resources/js/components/_history.js
+++ resources/js/components/_history.js
@@ -7,5 +7,7 @@
 var historyTooltips = document.querySelectorAll('.history-tooltip')
 
 Array.prototype.forEach.call(historyTooltips, function(el, i) {
-    var tooltip = new Bootstrap.Tooltip(el)
+    var tooltip = new Bootstrap.Tooltip(el, {
+      trigger: 'click'
+    })
 });
