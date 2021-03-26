767b6e621cdc04af2e2ade689b4ec01a0c338c37
Natacha Herth <info@natachaherth.ch>
2021-03-26 15:46:56 +0100

    extra breadcrumb yield

--- resources/views/layouts/partials/breadcrumb.blade.php
+++ resources/views/layouts/partials/breadcrumb.blade.php
@@ -1,3 +1,5 @@
 @if($hasBreadcrumb)
-  <x-bs-breadcrumb id="breadcrumb" :items="$crumbs"/>
+  <x-bs-breadcrumb id="breadcrumb" :items="$crumbs">
+    @yield('extraBreadcrumb')
+  </x-bs-breadcrumb>
 @endif
