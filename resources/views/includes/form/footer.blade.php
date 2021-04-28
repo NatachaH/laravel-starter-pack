a08111056ff33b68e36401f4c98f1797f356ee30
Natacha Herth <info@natachaherth.ch>
2021-04-28 10:09:43 +0200

    clean

--- resources/views/includes/form/footer.blade.php
+++ resources/views/includes/form/footer.blade.php
@@ -1,10 +1,10 @@
 <div class="form-footer row">
 
-  <div class="col-sm-6 order-sm-2 align-self-center mb-3 mb-sm-0 text-sm-end">
-    <p class="text-muted">@lang('sp::help.required') | @lang('sp::help.required-if')</p>
+  <div class="col-sm-8 order-sm-2 align-self-center mb-3 mb-sm-0 text-sm-end">
+    <p class="form-legend">@lang('sp::help.required') | @lang('sp::help.required-if')</p>
   </div>
 
-  <div class="col-sm-6 order-sm-1">
+  <div class="col-sm-4 order-sm-1">
     <button type="submit" class="btn btn-primary rounded-pill">@lang('sp::action.save')</button>
     <a href="{{ route($cancel,$cancelOption ?? null) }}" class="btn btn-outline-secondary rounded-pill">@lang('sp::action.cancel')</a>
   </div>
