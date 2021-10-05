978a8734552d0c2190995a8f93c98ab6954674ca
Natacha Herth <info@natachaherth.ch>
2021-10-05 10:59:38 +0200

    Corr if date is missing don't use format

--- resources/views/components/listing.blade.php
+++ resources/views/components/listing.blade.php
@@ -42,17 +42,13 @@
         @if($items->count())
 
           <div class="table-responsive">
-            <table class="table @if($withChildren) table-tree @endif">
+            <table class="table">
 
                 <thead>
                     <tr>
 
                         @if($isSortable())
                             <th class="td-fit"></th>
-                        @endif
-
-                        @if($withChildren)
-                          <th></th>
                         @endif
 
                         @if($showId)
@@ -90,7 +86,56 @@
 
                 <tbody @if($isSortable()) class="sortable" data-sortable-model="{{ $model }}" data-sortable-order="{{ $sortableOrder }}" @endif >
                     @foreach ($items as $item)
-                      @include('sp::includes.listing.item')
+                        <tr data-id="{{ $item->id }}">
+
+                            @if($isSortable())
+                                <td class="td-fit"><i class="icon-move drag"></i></td>
+                            @endif
+
+                            @if($showId)
+                                <td class="td-fit">{{ $item->id }}</td>
+                            @endif
+
+                            @includeFirst([$folder.'.includes.listing-detail', 'sp::includes.listing-detail'])
+
+                            @if($showDates('created'))
+                              <td class="td-fit d-none d-lg-table-cell">{{ $item->created_at ? $item->created_at->format('d.m.Y H:i') : '-' }}</td>
+                            @endif
+
+                            @if($showDates('updated'))
+                              <td class="td-fit d-none d-lg-table-cell">{{ $item->updated_at ? $item->updated_at->format('d.m.Y H:i') : '-' }}</td>
+                            @endif
+
+                            @if($showDates('deleted') && $hasSoftDelete())
+                              <td class="td-fit d-none d-lg-table-cell">{{ empty($item->deleted_at) ? '-' : $item->deleted_at->format('d.m.Y H:i') }}</td>
+                            @endif
+
+                            <td class="td-fit">
+
+                                @includeIf($folder.'.includes.listing-button')
+
+                                @if(Route::has($route.'.show') && Auth::user()->can('view', $item))
+                                    <a href="{{ route($route.'.show', $item->id) }}" class="btn btn-gray rounded-circle" aria-label="@lang('sp::action.preview')" title="@lang('sp::action.preview')"><i class="icon icon-preview"></i></a>
+                                @endif
+
+                                @if(Route::has($route.'.edit') && Auth::user()->can('update', $item))
+                                    <a href="{{ route($route.'.edit', $item->id) }}" class="btn btn-gray rounded-circle" aria-label="@lang('sp::action.edit')" title="@lang('sp::action.edit')"><i class="icon icon-pencil"></i></a>
+                                @endif
+
+                                @if(Route::has($route.'.destroy') && Auth::user()->can('delete', $item))
+                                    <button type="button" data-bs-toggle="modal" data-action="{{ route($route.'.destroy', $item->id) }}" data-bs-target="#{{ Route::has($route.'.forceDelete') ? 'softDeleteConfirm' : 'deleteConfirm' }}" class="btn btn-gray rounded-circle" aria-label="@lang('sp::action.delete')" title="@lang('sp::action.delete')"><i class="icon icon-trash"></i></button>
+                                @endif
+
+                                @if(Route::has($route.'.restore') && Auth::user()->can('restore', $item))
+                                    <button type="button" data-bs-toggle="modal" data-action="{{ route($route.'.restore', $item->id) }}" data-bs-target="#restoreConfirm" class="btn btn-gray rounded-circle" aria-label="@lang('sp::action.restore')" title="@lang('sp::action.restore')"><i class="icon icon-time-reverse"></i></button>
+                                @endif
+
+                                @if(Route::has($route.'.forceDelete') && Auth::user()->can('forceDelete', $item))
+                                    <button type="button" data-bs-toggle="modal" data-action="{{ route($route.'.forceDelete', $item->id) }}" data-bs-target="#deleteConfirm" class="btn btn-gray rounded-circle" aria-label="@lang('sp::action.force-delete')" title="@lang('sp::action.force-delete')"><i class="icon icon-trash"></i></button>
+                                @endif
+
+                            </td>
+                        </tr>
                     @endforeach
                 </tbody>
 
