5e58336835cd960276aca52c0501b5f918ffcbb4
Natacha Herth <info@natachaherth.ch>
2023-01-19 11:45:20 +0100

    remove tabletree

--- resources/js/components/components.js
+++ resources/js/components/components.js
@@ -8,11 +8,4 @@ import './_dynamic-media';
 import './_history';
 import './_popover-media';
 import './_sortable';
-import TableTree from './_table-tree';
 
-// Init the TableTreeView
-var tree = document.querySelectorAll('.table-tree');
-if(tree)
-{
-    tree.forEach(el => new TableTree(el));
-}
